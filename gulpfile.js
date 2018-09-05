84b48926b0ef3ef1f192d08a069ac6cc036e3a9d
Andy Lulham <a.lulham@gmail.com>
2018-09-05 23:34:19 +0100

    Minify css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp')
 var del = require('del')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
+const cleanCSS = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
 
 const outPath = './dist'
 
@@ -25,8 +27,11 @@ gulp.task('build:_bg_js', () => {
 gulp.task('build:js', gulp.parallel('build:_core_js', 'build:_bg_js'))
 
 gulp.task('build:css', () => {
-  return gulp.src(['./src/css/**', './node_modules/bootstrap/dist/css/*.min.css'])
+  return gulp.src(['./src/css/**', './node_modules/bootstrap/dist/css/bootstrap.css'])
     .pipe(concat('css.css'))
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(outPath + '/css'))
 })
 
