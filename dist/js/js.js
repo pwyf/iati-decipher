28ea42983605d1ab441a6fb7403cc94a1e512b0e
Andy Lulham <a.lulham@gmail.com>
2018-09-10 16:34:00 +0100

    Dist update

--- dist/js/js.js
+++ dist/js/js.js
@@ -29254,29 +29254,31 @@ module.exports = function(Chart) {
 
 })));
 
+const commify = function (x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
 var runApp = function (xml) {
   var orgs = $('iati-organisations iati-organisation', xml)
-  var $currentOrg = orgs[0]
+  // TODO: add an org switcher if the file declares
+  // multiple `iati-organisation`s
+  var $currentOrg = $(orgs[0])
 
+  var defaultCurrency = $currentOrg.attr('default-currency')
   var xLabels = []
   var data = []
 
-  console.log(moment)
-
   $('total-budget', $currentOrg).each(function () {
-    $this = $(this)
-    console.log($this)
+    var $this = $(this)
     var start = moment($('period-start', $this).attr('iso-date'))
     var end = moment($('period-end', $this).attr('iso-date'))
     if (!(start.isValid() || end.isValid())) {
       return
     }
-    console.log(end > start)
     var label = start.format('MMM YYYY') + ' – ' + end.format('MMM YYYY')
     xLabels.push(label)
     var d = $('> value', $this).text()
     data.push(d)
-    console.log(d)
   })
 
   var ctx = $('#chart')[0].getContext('2d')
@@ -29290,8 +29292,25 @@ var runApp = function (xml) {
       }]
     },
     options: {
+      tooltips: {
+        callbacks: {
+          label: function (tooltipItem, data) {
+            var label = data.datasets[0].label
+            return label + ': ' + commify(tooltipItem.xLabel) + ' ' + defaultCurrency
+          }
+        }
+      },
       legend: {
         display: false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            callback: function (value) {
+              return commify(value) + ' ' + defaultCurrency
+            }
+          }
+        }]
       }
     }
   })
