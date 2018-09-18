a47394599279fe68534c20d2e5287b97a70990a0
Andy Lulham <a.lulham@gmail.com>
2018-09-18 13:10:37 +0100

    Update dist

--- dist/js/background.js
+++ dist/js/background.js
@@ -1,15 +1,1 @@
-chrome.runtime.onMessage.addListener(function (request, sender, reply) {
-  switch (request.action) {
-    case 'msg.httprequest':
-      var xhr = new XMLHttpRequest()
-      xhr.open('GET', request.url, true)
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          reply({txt: xhr.responseText})
-        }
-      }
-      xhr.send()
-      break
-  }
-  return true
-})
+chrome.runtime.onMessage.addListener(function(e,t,n){switch(e.action){case"msg.httprequest":var s=new XMLHttpRequest;s.open("GET",e.url,!0),s.onreadystatechange=function(){4===s.readyState&&n({txt:s.responseText})},s.send()}return!0});
