32495b57acf0d2edba72017db68e914332831892
Andy Lulham <a.lulham@gmail.com>
2018-09-23 17:26:49 +0100

    Refactor to work on publisher pages, too.

--- src/js/action.js
+++ src/js/action.js
@@ -15,45 +15,87 @@ var navbarSelect = function (id) {
   $('#' + id).parent().addClass('active')
 }
 
+var setupHandlers = function (html, $org) {
+  // Total budget menu item
+  $('#show-total-budget').on('click', function () {
+    navbarSelect('show-total-budget')
+    showTotalBudget($org)
+    return false
+  })
+
+  // Org budget menu item
+  $('#show-org-budgets').on('click', function () {
+    navbarSelect('show-org-budgets')
+    showOrgBudgets($org)
+    return false
+  })
+
+  // Region budget menu item
+  $('#show-region-budgets').on('click', function () {
+    navbarSelect('show-region-budgets')
+    showRegionBudgets($org)
+    return false
+  })
+
+  // Country budget menu item
+  $('#show-country-budgets').on('click', function () {
+    navbarSelect('show-country-budgets')
+    showCountryBudgets($org)
+    return false
+  })
+
+  // Total expenditure menu item
+  $('#show-total-expenditure').on('click', function () {
+    navbarSelect('show-total-expenditure')
+    showTotalExpenditure($org)
+    return false
+  })
+
+  // Documents menu item
+  $('#show-documents').on('click', function () {
+    navbarSelect('show-documents')
+    showDocuments($org)
+    return false
+  })
+}
+
 $(function () {
-  // Check dataset metadata has file type "Organisation"
-  var fileType = $('section.additional-info th:contains("File Type")')
-    .parent()
-    .find('td.dataset-details')
-    .text()
-    .trim()
-  // If metadata doens't say it's an organisation file, bail.
-  if (fileType !== 'Organisation') {
-    return
+  if (window.location.toString().split('/')[3] === 'publisher') {
+    var $datasets = $('.dataset-content')
+    if ($datasets.length > 0) {
+      // this is a publisher page
+      var searchStr = 'No. of Activities:'
+      $datasets.each(function () {
+        var $dataset = $(this)
+        if ($dataset.text().indexOf(searchStr) === -1) {
+          var downloadUrl = $('p a:contains("Download")', $dataset).attr('href')
+          $('p a', $dataset).parent().append(' · ').append($('<a class="pwyf-org-viz-btn" data-download-url="' + downloadUrl + '" href="#">Visualise! <i class="icon-bar-chart"></i></a>'))
+        }
+      })
+    }
+  } else {
+    // this is a dataset page
+    //
+    // Check dataset metadata has file type "Organisation"
+    var fileType = $('section.additional-info th:contains("File Type")')
+      .parent()
+      .find('td.dataset-details')
+      .text()
+      .trim()
+    // Check if metadata says it's an organisation file.
+    if (fileType === 'Organisation') {
+      // Find the download button on the page, and
+      // get the dataset URL
+      var downloadUrl = $($('.resources .btn-primary')[0]).attr('href')
+
+      // Inject a 'Visualise!' button onto the page
+      var btns = $('.resources').find('li')
+      btns.append(' · ').append($('<a href="#" data-download-url="' + downloadUrl + '" class="btn btn-danger pwyf-org-viz-btn"><i class="icon-bar-chart"></i> Visualise!</a>'))
+    }
   }
 
-  // Find the download button on the page, and
-  // get the dataset URL
-  var xmlUrl = $($('.btn-primary')[0]).attr('href')
-
-  // Inject a 'Visualise!' button onto the page
-  var btns = $('.resources').find('li')
-  btns.append(' · ').append($('<a id="pwyf-org-viz-btn" href="#" class="btn btn-danger disabled"><i class="icon-bar-chart"></i> Visualise!</a>'))
-
-  // Save the current DOM, so we can recover it later
-  var oldDom = document.documentElement
-
-  // Download the dataset
-  chrome.runtime.sendMessage({action: 'msg.httprequest', url: xmlUrl}, function (response) {
-    // Parse the dataset
-    var xml = new DOMParser().parseFromString(response.txt, 'application/xml')
-
-    // if the root node is wrong, bail.
-    if ($(':root', xml)[0].nodeName !== 'iati-organisations') {
-      return
-    }
-
-    var version = $('iati-organisations', xml).attr('version')
-    var $orgs = $('iati-organisations iati-organisation', xml)
-    // TODO: add an org switcher if the file declares
-    // multiple `iati-organisation`s. This is pretty unusual,
-    // though
-    var $org = $($orgs[0])
+  $('.pwyf-org-viz-btn', 'body').on('click', function () {
+    var downloadUrl = $(this).data('download-url')
 
     // Fetch our template
     chrome.runtime.sendMessage({action: 'msg.httprequest', url: chrome.extension.getURL('html/html.html')}, function (response) {
@@ -62,66 +104,46 @@ $(function () {
       txt = txt.replace(/{path:([^}]+)}/g, function (_, assetPath) {
         return chrome.extension.getURL(assetPath)
       })
+      // Save the current DOM, so we can recover it later
+      var oldDom = document.documentElement
       // Parse our template
       var html = new DOMParser().parseFromString(txt, 'text/html')
       var newDom = html.documentElement
-
-      // Total budget menu item
-      $('#show-total-budget', $('body', html)).on('click', function () {
-        navbarSelect('show-total-budget')
-        showTotalBudget($org)
-        return false
-      })
-
-      // Org budget menu item
-      $('#show-org-budgets', $('body', html)).on('click', function () {
-        navbarSelect('show-org-budgets')
-        showOrgBudgets($org)
-        return false
-      })
-
-      // Region budget menu item
-      $('#show-region-budgets', $('body', html)).on('click', function () {
-        navbarSelect('show-region-budgets')
-        showRegionBudgets($org)
-        return false
-      })
-
-      // Country budget menu item
-      $('#show-country-budgets', $('body', html)).on('click', function () {
-        navbarSelect('show-country-budgets')
-        showCountryBudgets($org)
-        return false
-      })
-
-      // Total expenditure menu item
-      $('#show-total-expenditure', $('body', html)).on('click', function () {
-        navbarSelect('show-total-expenditure')
-        showTotalExpenditure($org)
-        return false
-      })
-
-      // Documents menu item
-      $('#show-documents', $('body', html)).on('click', function () {
-        navbarSelect('show-documents')
-        showDocuments($org)
-        return false
-      })
+      document.replaceChild(document.adoptNode(newDom), document.documentElement)
 
       // Reset button
-      $('#revert', $('body', html)).on('click', function () {
+      $('#revert').on('click', function () {
         document.replaceChild(document.adoptNode(oldDom), document.documentElement)
         return false
       })
 
-      $('#pwyf-org-viz-btn', 'body').on('click', function () {
-        document.replaceChild(document.adoptNode(newDom), document.documentElement)
+      // Download the dataset
+      chrome.runtime.sendMessage({action: 'msg.httprequest', url: downloadUrl}, function (response) {
+        // Parse the dataset
+        var xml = new DOMParser().parseFromString(response.txt, 'application/xml')
+
+        // if the root node is wrong, bail.
+        if ($(':root', xml)[0].nodeName !== 'iati-organisations') {
+          return
+        }
+
+        var $orgs = $('iati-organisations iati-organisation', xml)
+        // TODO: add an org switcher if the file declares
+        // multiple `iati-organisation`s. This is pretty unusual,
+        // though
+        var $org = $($orgs[0])
+
+        setupHandlers(html, $org)
+
+        var version = $('iati-organisations', xml).attr('version')
         $('#org-name').text(getOrgName($org, version))
+
         // Run the visualize app
         navbarSelect('show-total-budget')
         showTotalBudget($org)
-        return false
-      }).removeClass('disabled')
+      })
     })
+
+    return false
   })
 })
