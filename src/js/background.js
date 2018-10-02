c15dbe075b0746a412854f85f6867bafc6c0b320
Andy Lulham <a.lulham@gmail.com>
2018-10-02 17:43:34 +0100

    Promisify the background script

--- src/js/background.js
+++ src/js/background.js
@@ -1,18 +1,47 @@
-chrome.runtime.onMessage.addListener(function (request, sender, reply) {
-  var xhr = new XMLHttpRequest()
-  if (request.action === 'msg.jsonrequest') {
-    xhr.responseType = 'json'
-  }
-  xhr.open('GET', request.url, true)
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (request.action === 'msg.httprequest') {
-        reply(xhr.responseText)
-      } else if (request.action === 'msg.jsonrequest') {
-        reply(xhr.response)
+var get = function (url, responseType) {
+  // Return a new promise.
+  return new Promise(function (resolve, reject) {
+    var req = new XMLHttpRequest()
+    if (responseType === 'json') {
+      req.responseType = responseType
+    }
+    req.open('GET', url)
+
+    req.onload = function () {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status === 200) {
+        // Resolve the promise with the response text
+        resolve(req)
+      } else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText))
       }
     }
+
+    // Handle network errors
+    req.onerror = function () {
+      reject(Error('Network Error'))
+    }
+
+    // Make the request
+    req.send()
+  })
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, reply) {
+  var responseType = null
+  if (request.action === 'msg.jsonrequest') {
+    responseType = 'json'
   }
-  xhr.send()
+  get(request.url, responseType).then(function (response) {
+    if (request.action === 'msg.httprequest') {
+      reply(response.responseText)
+    } else if (request.action === 'msg.jsonrequest') {
+      reply(response.response)
+    }
+  })
+
   return true
 })
