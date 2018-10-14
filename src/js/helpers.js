53ac654ab1fae8d6d3a95f8789dc8ffa6ee59afd
Andy Lulham <a.lulham@gmail.com>
2018-10-14 15:32:13 +0100

    Remove cruft

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -4,26 +4,6 @@ $.extend($.expr[':'], {
   }
 })
 
-// numeral.register('locale', 'pwyf', {
-//   delimiters: {
-//     thousands: ',',
-//     decimal: '.'
-//   },
-//   abbreviations: {
-//     thousand: 'thousand',
-//     million: 'million',
-//     billion: 'billion',
-//     trillion: 'trillion'
-//   },
-//   ordinal: function () {
-//     return ''
-//   },
-//   currency: {
-//     symbol: ''
-//   }
-// })
-// numeral.locale('pwyf')
-
 var getOrgName = function ($org, version) {
   var $names
   if (version[0] === '2') {
