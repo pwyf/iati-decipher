1312f83da4d394ed39d925db327ecc780c52f7ac
Andy Lulham <a.lulham@gmail.com>
2018-10-14 09:12:16 +0100

    Debounce to save hammering the registry API

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,23 @@
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce (func, wait, immediate) {
+  var timeout
+  return function () {
+    var context = this
+    var args = arguments
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
+
 $(function () {
   $('body').on('click', 'a.list-group-item', function () {
     chrome.runtime.sendMessage({action: 'msg.opentab', url: $(this).attr('href')})
@@ -5,8 +25,7 @@ $(function () {
   })
 
   var tmpl = 'https://iatiregistry.org/api/3/action/package_search?fq=extras_filetype:organisation&q=title:'
-  $('#org-file-name').on('keyup', function () {
-
+  $('#org-file-name').on('keyup', debounce(function () {
     var searchStr = $(this).val()
     searchStr = searchStr.split(' ')
     searchStr = '(+*' + searchStr.join('* +*') + '*)'
@@ -18,5 +37,5 @@ $(function () {
       })
       $('.list-group').html(links.join(''))
     })
-  })
+  }, 500))
 })
