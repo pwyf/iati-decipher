6c1acff7b8625082f655c586b308a076a34f5920
Kevin Damato <kevindamato889@gmail.com>
2018-11-23 04:26:54 +0900

    Expanded search query

--- src/js/popup.js
+++ src/js/popup.js
@@ -8,7 +8,22 @@ $(function () {
     return false
   })
 
-  var tmpl = 'https://iatiregistry.org/api/3/action/package_search?fq=extras_filetype:organisation&qf=title&q='
+
+  var tmpl = 'https://iatiregistry.org/api/3/action/package_search?fq=extras_filetype:organisation&q=title:'
+  var tmpl2 = ' OR license_id:'
+  var tmpl3 = ' OR maintainer:'
+  var tmpl4 = ' OR maintainer_email:'
+  var tmpl5 = ' OR author_email:'
+  var tmpl6 = ' OR name:'
+  var tmpl7 = ' OR url:'
+  var tmpl8 = ' OR resources_url:'
+  var tmpl9 = ' OR organization_name:'
+  var tmpl10 = ' OR organization_image_url:'
+  var tmpl11 = ' OR organization_description:'
+  var tmpl12 = ' OR extras_country:'
+  var tmpl13 = ' OR extras_publisher_country:'
+  var tmpl14 = ' OR extras_publisher_iati_id:'
+
   $('#org-file-name').on('keyup', debounce(function () {
     var searchStr = $(this).val()
     if (searchStr === '') {
@@ -16,7 +31,7 @@ $(function () {
       return
     }
     $('.list-group').html('<div id="mini-spinner"><div></div></div>')
-    chrome.runtime.sendMessage({action: 'msg.jsonrequest', url: tmpl + searchStr}, function (data) {
+    chrome.runtime.sendMessage({action: 'msg.jsonrequest', url: tmpl + searchStr + tmpl2 + searchStr + tmpl3 + searchStr + tmpl4 + searchStr + tmpl5 + searchStr + tmpl6 + searchStr + tmpl7 + searchStr + tmpl8 + searchStr + tmpl9 + searchStr + tmpl10 + searchStr + tmpl11 + searchStr + tmpl12 + searchStr + tmpl13 + searchStr + tmpl14 + searchStr}, function (data) {
       var result = data.message.result
       var links = []
       if (result.count > 0) {
